74d29dcd7dbda2a411a9b4b1c88f08cd9b42a8e0
Sallyyyyyyyyyyyyy123455 <lesika161@gmail.com>
2026-06-09 16:24:42 +0300

    Update vigenere.h

--- Server/vigenere.h
+++ Server/vigenere.h
@@ -1,1 +1,9 @@
+#ifndef VIGENERE_H
+#define VIGENERE_H
 
+#include <string>
+
+std::string encryptVigenere(const std::string& text, const std::string& key);
+std::string decryptVigenere(const std::string& text, const std::string& key);
+
+#endif
