23326324c812b400a4ba65486a3ae91b36c25e5f
Sallyyyyyyyyyyyyy123455 <lesika161@gmail.com>
2026-06-09 16:28:26 +0300

    Update test_vigenere.cpp

--- Test/test_vigenere.cpp
+++ Test/test_vigenere.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cassert>
 
+std::string encrypt(const std::string& text, const std::string& key) {
+    std::string res;
+    for (size_t i = 0; i < text.size(); i++)
+        res += text[i] + key[i % key.size()];
+    return res;
+}
+
+std::string decrypt(const std::string& text, const std::string& key) {
+    std::string res;
+    for (size_t i = 0; i < text.size(); i++)
+        res += text[i] - key[i % key.size()];
+    return res;
+}
+
+int main() {
+    std::string plain = "HELLO";
+    std::string key = "KEY";
+    std::string enc = encrypt(plain, key);
+    std::string dec = decrypt(enc, key);
+    assert(dec == plain);
+    std::cout << "Test passed!\n";
+    return 0;
+}
