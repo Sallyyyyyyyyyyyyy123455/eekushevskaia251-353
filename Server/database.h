b3365a033a0043399a134a2c8c96a351ed88c040
Sallyyyyyyyyyyyyy123455 <lesika161@gmail.com>
2026-06-09 16:23:51 +0300

    Update database.h

--- Server/database.h
+++ Server/database.h
@@ -1,1 +1,12 @@
+#ifndef DATABASE_H
+#define DATABASE_H
 
+#include <string>
+#include <sqlite3.h>
+
+bool initDatabase();
+bool registerUser(const std::string& username, const std::string& password);
+int loginUser(const std::string& username, const std::string& password);
+std::string hashPassword(const std::string& password);
+
+#endif
